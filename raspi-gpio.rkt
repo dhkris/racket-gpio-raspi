abb6bb85035254e138a1c1a5eb9eecb4f48834cc
David Christensen <dch@dis-play.dk>
2016-06-19 21:52:39 +0200

    Added helpers, improved types

--- raspi-gpio.rkt
+++ raspi-gpio.rkt
@@ -23,64 +23,103 @@
 
 ;; Setup
 (define gpio-setup
-  (get-ffi-obj "wiringPiSetup" wiringpi-lib (_fun _void -> _int)
+  (get-ffi-obj "wiringPiSetup" wiringpi-lib (_fun -> _int32)
                (lambda () (error 'wiringpi-lib "WiringPI does not provide wiringPiSetup"))))
 
-(define gpio-pin-mode
-  (get-ffi-obj "pinMode" wiringpi-lib (_fun _int _int -> _void)
+(define gpio-pin-mode-native
+  (get-ffi-obj "pinMode" wiringpi-lib (_fun _int32 _int32 -> _void)
                (lambda () (error 'wiringpi-lib "pinMode missing"))))
 
 (define gpio-pull-up-down-control
-  (get-ffi-obj "pullUpDnControl" wiringpi-lib (_fun _int _int -> _void)
+  (get-ffi-obj "pullUpDnControl" wiringpi-lib (_fun _int32 _int32 -> _void)
                (lambda () (error 'wiringpi-lib "pullUpDnControl missing"))))
 
 ;; Digital interfacing
 (define gpio-digital-write
-  (get-ffi-obj "digitalWrite" wiringpi-lib (_fun _int _int -> _void)
+  (get-ffi-obj "digitalWrite" wiringpi-lib (_fun _int32 _int32 -> _void)
                (lambda () (error 'wiringpi-lib "digitalWrite missing"))))
 
 (define gpio-digital-read
-  (get-ffi-obj "digitalRead" wiringpi-lib (_fun _int -> _int)
+  (get-ffi-obj "digitalRead" wiringpi-lib (_fun _int32 -> _int32)
                (lambda () (error 'wiringpi-lib "digitalRead missing"))))
 
 ;; PWM 
 (define gpio-pwm-write
-  (get-ffi-obj "pwmWrite" wiringpi-lib (_fun _int _int -> _void)
+  (get-ffi-obj "pwmWrite" wiringpi-lib (_fun _int32 _int32 -> _void)
                (lambda () (error 'wiringpi-lib "pwmWrite missing"))))
 
 ;; Analog interfacing
 (define gpio-analog-write
-  (get-ffi-obj "analogWrite" wiringpi-lib (_fun _int _int -> _void)
+  (get-ffi-obj "analogWrite" wiringpi-lib (_fun _int32 _int32 -> _void)
                (lambda () (error 'wiringpi-lib "digitalWrite missing"))))
 
 (define gpio-analog-read
-  (get-ffi-obj "analogRead" wiringpi-lib (_fun _int -> _int)
+  (get-ffi-obj "analogRead" wiringpi-lib (_fun _int32 -> _int32)
                (lambda () (error 'wiringpi-lib "digitalRead missing"))))
 
 ;;; Timing
 (define gpio-elapsed-ms
-  (get-ffi-obj "millis" wiringpi-lib (_fun _void -> _uint)
+  (get-ffi-obj "millis" wiringpi-lib (_fun -> _uint32)
                (lambda () (error 'wiringpi-lib "millis missing"))))
 
 (define gpio-elapsed-µs
-  (get-ffi-obj "micros" wiringpi-lib (_fun _void -> _uint)
+  (get-ffi-obj "micros" wiringpi-lib (_fun -> _uint32)
                (lambda () (error 'wiringpi-lib "micros missing"))))
 
 (define gpio-delay-ms
-  (get-ffi-obj "delay" wiringpi-lib (_fun _uint -> _void)
+  (get-ffi-obj "delay" wiringpi-lib (_fun _uint32 -> _void)
                (lambda () (error 'wiringpi-lib "delay missing"))))
 
 (define gpio-delay-µs
-  (get-ffi-obj "delayMicroseconds" wiringpi-lib (_fun _uint -> _void)
+  (get-ffi-obj "delayMicroseconds" wiringpi-lib (_fun _uint32 -> _void)
                (lambda () (error 'wiringpi-lib "delayMicroseconds missing"))))
+
+
+;; Pin mode defines
+(define gpio-pin-input 0)
+(define gpio-pin-output 1)
+(define gpio-pin-pwm 2)
+(define gpio-pin-clock 3)
+(define gpio-pin-soft-pwm 4)
+(define gpio-pin-soft-tone 5)
+(define gpio-pin-pwm-tone 6)
+
+;; Pullup/down modes
+(define gpio-pull-up 2)
+(define gpio-pull-down 1)
+(define gpio-pull-off 0)
 
 
 
 ;;; Custom helpers
+;; More "native" pin mode setter
+(define gpio-set-pin-mode
+  (lambda (pin mode)
+    (cond
+      [(= mode 'input) (gpio-pin-mode-native pin gpio-pin-input)]
+      [(= mode 'output) (gpio-pin-mode-native pin gpio-pin-output)]
+      [(= mode 'pwm) (gpio-pin-mode-native pin gpio-pin-pwm)]
+      [(= mode 'clock) (gpio-pin-mode-native pin gpio-pin-clock)]
+      [(= mode 'soft-pwm) (gpio-pin-mode-native pin gpio-pin-soft-pwm)]
+      [(= mode 'soft-tone) (gpio-pin-mode-native pin gpio-pin-soft-tone)]
+      [(= mode 'pwm-tone) (gpio-pin-mode-native pin gpio-pin-pwm-tone)]
+      [else (error 'gpio-set-pin-mode (string-join "Erroneous pin mode passed: " (symbol->string mode) ". Valid: 'input 'output 'pwm 'clock 'soft-pwm 'soft-tone 'pwm-tone"))])))
+
+(define gpio-set-pull-resistor
+  (lambda (pin mode)
+    (cond
+      [(= mode 'up) (gpio-pull-up-down-control pin gpio-pull-up)]
+      [(= mode 'down) (gpio-pull-up-down-control pin gpio-pull-down)]
+      [(= mode 'off) (gpio-pull-up-down-control pin gpio-pull-off)]
+      [else (error 'gpio-set-pull-resistor (string-join "Erroneous pullup/down mode passed: " (symbol->string mode) ". Valid: 'up, 'down, 'off'"))])))
+                            
+
+;; GPIO delay
 (define gpio-delay-seconds
   (lambda (seconds)
     (gpio-delay-µs (floor (* seconds 1000000)))))
 
+;; Write a byte of data sequentially to specified pin
 (define gpio-write-serial-byte
   (lambda (pin byte bit-duration)
     (letrec ([gpio-write-byte-op
